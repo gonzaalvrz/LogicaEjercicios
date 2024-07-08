203eda8809aaace86dffb95263987eb68fb4967f
gonzaalvrz <109773660+gonzaalvrz@users.noreply.github.com>
2024-07-08 16:55:39 -0300

    Update main.cpp
    Numero perfecto y guay

--- Lógica/main.cpp
+++ Lógica/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 /*General*/
  /*
 int Resta(int a, int b){
-  cout << "ingrese primer número:" << endl;
+  cout << "ingrese primer nÃºmero:" << endl;
   cin >> a;
-  cout << "ingrese segundo número:" << endl;
+  cout << "ingrese segundo nÃºmero:" << endl;
   cin >> b;
   cout << "Resultado: ";
   int value = 0;
@@ -64,6 +64,42 @@ int numero(int a) {
     return value;
 
 }*/
+
+
+
+/*
+int numerosPerfecto(int a) {
+    int value = 0;
+    for (int i = 1; i < a; i++) {
+        if (a % i == 0) {
+            value += i;
+            if (value == a) cout << a << " es perfecto" << endl;
+            else cout << a << " no es perfecto" << endl; 
+       }
+       return value;
+    }  
+}
+*/
+
+/*
+int numeroGuay(int a) {
+    int value = 0;
+    bool guay = false;
+
+    for (int i = 1; i < a && guay == false; i++) {
+        value += i;
+        if (value == a) {
+         guay = true; 
+         if (guay) cout << a << " Es un numero Guay";
+         else cout << a << " No es un numero Guay";
+        }
+    }
+    return value;
+}
+*/
+
+
+
 int main() {
     int a = 0, b = 0;
     //cout << numero(10);
@@ -71,5 +107,7 @@ int main() {
     //cout << "Multiplicacion: " << Multiplicacion(3, 6) << endl;
     //cout << "Division: " << Division(10, 2) << endl;
     //cout << "Potencia: " << Potencia(8, 2) << endl;
+    //numerosPerfecto(10);
+    //numeroGuay(8);
     return 0;
 }
